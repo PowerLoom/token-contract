2570df7be7a85bf85ad95570e9a61fd53d0554cb
Swaroop Hegde <email@swaroophegde.com>
2023-11-28 00:45:05 -0800

    Create PowerloomToken.sol

---
+++ PowerloomToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC20/extensions/ERC20Votes.sol";
+
+/// @custom:security-contact security@powerloom.network
+contract PowerloomToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor()
+        ERC20("Powerloom Token", "POWER")
+        ERC20Permit("Powerloom Token")
+    {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
